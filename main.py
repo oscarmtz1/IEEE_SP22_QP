79d696224aa1435a12dfc7c330ace717716f0199
Oscar <osmartin@ucsd.edu>
2022-04-24 15:14:02 -0700

    added virtual environment

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+import pathlib
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+np.set_printoptions(precision=4)
+
+dataset = tf.data.Dataset.from_tensor_slices([8, 3, 0, 8, 2, 1])
+dataset
+
+
+
 print("hello world!")
+
