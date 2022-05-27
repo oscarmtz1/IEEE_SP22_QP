5388726afe87eda396e6949cd892f7a2cf1429f2
oscarmtz1 <osmartin@ucsd.edu>
2022-05-27 01:35:48 -0700

    fixed script to include tai_prediction, takes in args from command line

--- classifier.py
+++ classifier.py
@@ -6,6 +6,34 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
+import sys
+
+# Sampling rate.
+sr = 22050
+
+# Let's make sure all files have the same amount of samples and pick a duration right under 30 seconds.
+TOTAL_SAMPLES = 29 * sr
+
+# The dataset contains 999 files. Lets make it bigger. 
+# X amount of slices => X times more training examples.
+NUM_SLICES = 10
+SAMPLES_PER_SLICE = int(TOTAL_SAMPLES / NUM_SLICES)
+
+NUM_SLICES = 10
+SAMPLES_PER_SLICE = int(TOTAL_SAMPLES / NUM_SLICES)
+
+def prepare_datasets(inputs, targets, split_size):
+    
+    # Creating a validation set and a test set.
+    inputs_train, inputs_val, targets_train, targets_val = train_test_split(inputs, targets, test_size=split_size)
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs_train, targets_train, test_size=split_size)
+    
+    # Our CNN model expects 3D input shape.
+    inputs_train = inputs_train[..., np.newaxis]
+    inputs_val = inputs_val[..., np.newaxis]
+    inputs_test = inputs_test[..., np.newaxis]
+    
+    return inputs_train, inputs_val, inputs_test, targets_train, targets_val, targets_test
 
 def load_data(json_path):
 
@@ -19,11 +47,71 @@ def load_data(json_path):
 
     return X, y
 
+def preprocess_data(source_path, json_path):
+
+    # Let's create a dictionary of labels and processed data.
+    mydict = {
+        "labels": [],
+        "mfcc": []
+        }
+
+    # Let's browse each file, slice it and generate the 13 band mfcc for each slice.
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(source_path)):
+        for file in filenames:
+            # exclude a corrupted wav file that makes everything crash.
+            if os.path.join(dirpath, file) != 'data\genres_original\jazz\jazz.00054.wav':
+                song, sr = librosa.load(os.path.join(dirpath, file), duration=29)
+                for s in range(NUM_SLICES):
+                    start_sample = SAMPLES_PER_SLICE * s
+                    end_sample = start_sample + SAMPLES_PER_SLICE
+                    mfcc = librosa.feature.mfcc(y=song[start_sample:end_sample], sr=sr, n_mfcc=13)
+                    mfcc = mfcc.T
+                    mydict["labels"].append(i-1)
+                    mydict["mfcc"].append(mfcc.tolist())
+            else:
+                pass
+
+    # Let's write the dictionary in a json file.    
+    with open(json_path, 'w') as f:
+        json.dump(mydict, f)
+    f.close()
+
+
 model = tf.keras.models.load_model('saved_model/my_model')
 
-music_file = ''
-music_target = ''
+def tai_prediction(model, X, y, idx):
+    
+    genre_dict = {
+        0 : "blues",
+        1 : "classical",
+        2 : "country",
+        3 : "disco",
+        4 : "hiphop",
+        5 : "jazz",
+        6 : "metal",
+        7 : "pop",
+        8 : "reggae",
+        9 : "rock",
+        }
+        
+    predictions = model.predict(X)
+    genre = np.argmax(predictions[idx])
+    
+    #print("\n---Now testing the model for one audio file---\nThe model predicts: {}, and ground truth is: {}.\n".format(genre_dict[genre], genre_dict[y[idx]]))
+    print("\n---Now testing the model for one audio file---\nThe model predicts: {}, but what was it actually? \n".format(genre_dict[genre]))
 
-inputs, targets = load_data(json_path=music_target)
-stuff = train_test_split(inputs, targets, test_size=0.2)
-model.predict(stuff[2])
+def super_prediction(path, target):
+    preprocess_data(source_path=path,json_path=target)
+    inputs, targets = load_data(json_path=path)
+
+    Xtrain, Xval, Xtest, ytrain, yval, ytest = prepare_datasets(inputs, targets, 0.2)
+    tai_prediction(model,Xtest,ytest,2)
+
+music_file = sys.argv[0]
+music_target = sys.argv[1]
+
+super_prediction(music_file, music_target)
+
+#inputs, targets = load_data(json_path=music_target)
+#stuff = train_test_split(inputs, targets, test_size=0.2)
+#model.predict(stuff[2])
